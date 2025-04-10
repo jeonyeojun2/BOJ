53167b2caa2294d58d73c84e2ccc18446af45fe1
Yeojun Jeon <duwns709@gmail.com>
2025-04-10 10:41:54 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 40 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -18,14 +18,14 @@ int main()
 
     vector<pair<int, int>> points(N);
 
-    for (int i = 0; i < N; ++i)
+    for (int i = 0; i < N; i++)
     {
         cin >> points[i].first >> points[i].second;
     }
 
     sort(points.begin(), points.end());
 
-    for (int i = 0; i < N; ++i)
+    for (int i = 0; i < N; i++)
     {
         cout << points[i].first << " " << points[i].second << "\n";
     }
