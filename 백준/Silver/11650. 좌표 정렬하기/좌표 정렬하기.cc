ecfa6dbbf9b7bbaa3b0ebd725cab7b9c3f2be11e
Yeojun Jeon <duwns709@gmail.com>
2025-04-10 10:16:26 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 40 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,32 @@
+// 11650
+#include <iostream>
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    vector<pair<int, int>> points(N);
+
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> points[i].first >> points[i].second;
+    }
+
+    sort(points.begin(), points.end());
+
+    for (int i = 0; i < N; ++i)
+    {
+        cout << points[i].first << " " << points[i].second << "\n";
+    }
+}
