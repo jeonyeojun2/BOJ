e8eaa7bcee00c557c0dc4bbcbe25a12cebb20873
Yeojun Jeon <duwns709@gmail.com>
2025-06-28 20:50:52 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -0,0 +1,27 @@
+// 2869
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int A, B, V;
+  cin >> A >> B >> V;
+
+  long long day = (V - B - 1) / (A - B) + 1;
+
+  cout << day;
+}
