eeeea14f3df5a09216df564020e324595358177e
Yeojun Jeon <duwns709@gmail.com>
2025-07-02 22:13:25 +0900

    [Silver V] Title: 덩치, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.cc
+++ 백준/Silver/7568. 덩치/덩치.cc
@@ -1,48 +1,31 @@
-// 7568
-#include <algorithm>
-#include <cmath>
-#include <cstring>
-#include <iomanip>
 #include <iostream>
-#include <map>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
 #include <vector>
 using namespace std;
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
+    int n;
+    cin >> n;
 
-  int N;
-  cin >> N;
+    vector<pair<int, int>> people(n); // (몸무게, 키) 저장
 
-  int array[N][3];
-
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < 2; j++) {
-      cin >> array[i][j];
-    }
-  }
-
-  for (int i = 0; i < N; i++) {
-    int count = 0;
-    int temp_w = array[i][0];
-    int temp_h = array[i][1];
-
-    for (int j = 0; j < N; j++) {
-      if (temp_w < array[j][0] && temp_h < array[j][1]) count++;
+    for (int i = 0; i < n; i++) {
+        cin >> people[i].first >> people[i].second;
     }
 
-    array[i][2] = count + 1;
-  }
+    for (int i = 0; i < n; i++) {
+        int rank = 1; // 자기 자신은 항상 등수 1에서 시작
 
-  for (int i = 0; i < N - 1; i++) {
-    cout << array[i][2] << " ";
-  }
-  cout << array[N - 1][2];
+        for (int j = 0; j < n; j++) {
+            if (i == j) continue;
+
+            // 상대방이 몸무게와 키 둘 다 큰 경우
+            if (people[j].first > people[i].first && people[j].second > people[i].second) {
+                rank++;
+            }
+        }
+
+        cout << rank << ' ';
+    }
+
+    return 0;
 }
