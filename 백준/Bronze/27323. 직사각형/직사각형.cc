942e20e4be4e7c84c08ad411fb7a39f1c583c105
Yeojun Jeon <duwns709@gmail.com>
2025-08-19 16:39:03 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,30 @@
+// 27323 : 직사각형
+// 수학
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, b;
+  cin >> a >> b;
+
+  cout << a * b;
+}
