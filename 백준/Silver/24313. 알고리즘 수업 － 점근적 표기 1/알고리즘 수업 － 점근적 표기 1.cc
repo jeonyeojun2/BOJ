52f73dab8fa8ca0bc1b33f4428acfe4e8da8c914
Yeojun Jeon <duwns709@gmail.com>
2025-08-21 15:43:53 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.cc
@@ -0,0 +1,36 @@
+// 24313 : 알고리즘 수업 - 점근적 표기 1
+// 수학
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  long long a1, a0, c, n0;
+  cin >> a1 >> a0 >> c >> n0;
+
+  long long f = a1 * n0 + a0;
+  long long g = c * n0;
+
+  if (a1 <= c && f <= g)
+    cout << 1;
+  else
+    cout << 0;
+}
