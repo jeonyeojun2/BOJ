41e0f7e8065bbca6f7aae842bbc13159158d17cb
Yeojun Jeon <duwns709@gmail.com>
2025-08-21 15:29:19 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cc
@@ -0,0 +1,33 @@
+// 24265 : 알고리즘 수업 - 알고리즘의 수행 시간 4
+// 시뮬레이션
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  long long n, result = 0;
+  cin >> n;
+
+  for (long long i = n - 1; i >= 1; i--) result += i;
+
+  cout << result << "\n";
+  cout << 2 << "\n";
+}
