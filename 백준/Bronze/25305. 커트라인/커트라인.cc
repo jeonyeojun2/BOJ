5cbd8243a375f264b61f293750f1893083855c4b
Yeojun Jeon <duwns709@gmail.com>
2025-08-22 15:13:36 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,35 @@
+// 25305 : 커트라인
+// 정렬
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, k;
+  cin >> n >> k;
+  vector<int> v(n);
+
+  for (int i = 0; i < n; i++) cin >> v[i];
+
+  sort(v.begin(), v.end());
+
+  cout << v[n - k];
+}
