28a8a63201f3a7e54a855f1c1d476fbf5e8c6a64
Yeojun Jeon <duwns709@gmail.com>
2026-01-13 11:52:41 +0900

    [Bronze V] Title: A+B - 5, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
@@ -1,6 +1,6 @@
+import sys
 while 1:
-    a, b = map(int, input().split())
-    if (a != 0 and b != 0):
-        print(a+b)
-    else :
+    a, b = map(int, sys.stdin.readline().split())
+    if a == 0 and b == 0:
         break
+    print(a + b)
