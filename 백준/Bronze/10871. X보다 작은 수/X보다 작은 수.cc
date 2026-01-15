2a02ad6905e5daa5c2c101ad683069e2f9d49eef
Yeojun Jeon <duwns709@gmail.com>
2026-01-15 16:05:33 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int n, x, a[10005];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> x;
+
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n; i++) {
+        if (a[i] < x)
+            cout << a[i] << " ";
+    }
+}
