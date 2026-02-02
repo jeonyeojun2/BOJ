5ae99c0ba41ba9488c1f63571e4eaa1dcc72584e
Yeojun Jeon <duwns709@gmail.com>
2026-02-03 00:49:36 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,44 +1,24 @@
-// 1629 : 곱셈
-// 분할 정복
-#include <algorithm>
-#include <climits>
-#include <cmath>
-#include <cstring>
-#include <deque>
-#include <iomanip>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-long long modPow(long long A, long long B, long long C) {
-  if (B == 0) return 1;
-  if (B == 1) return A % C;
+long long pow(long long a, long long b, long long m) {
+    if (b == 1)
+        return a % m;
 
-  // B를 절반으로 나누어 계산 (분할 정복)
-  long long half = modPow(A, B / 2, C);
-  long long result = (half * half) % C;
-
-  // B가 홀수라면 A를 한 번 더 곱함
-  if (B % 2 == 1) result = (result * (A % C)) % C;
-
-  return result;
+    long long val = pow(a, b / 2, m);
+    val = val * val % m;
+    if (b % 2 == 0)
+        return val;
+    return val * a % m;
 }
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-  long long A, B, C;
-  cin >> A >> B >> C;
+    long long a, b, c;
+    cin >> a >> b >> c;
 
-  cout << modPow(A, B, C);
+    cout << pow(a, b, c);
 }
